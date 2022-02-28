bdc96cef714098c3ef4c85877f99c82142cc5b94
Oğuz Karan <oguzkaran@csystem.org>
2022-02-28 13:13:18 +0300

    Update

---
+++ src/Libraries/MapperUtilLib/AutoMapper/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSD.Util.Mappers.AutoMapper
+{
+    public class Mapper : IMapper
+    {
+        public Mapper()
+        { 
+                                                
+        }
+
+        public D Map<D, S>(S source)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
